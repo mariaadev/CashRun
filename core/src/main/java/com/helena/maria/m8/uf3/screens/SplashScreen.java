4b4358aa4f747649672ea2461ad7af885bec0f1f
Maria Amzil <mini.mar@hotmail.com>
2025-05-08 19:09:56 +0200

    Realizada pantalla de SplashScreen

--- core/src/main/java/com/helena/maria/m8/uf3/screens/SplashScreen.java
+++ core/src/main/java/com/helena/maria/m8/uf3/screens/SplashScreen.java
@@ -12,6 +12,8 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.helena.maria.m8.uf3.CashRun;
 import com.helena.maria.m8.uf3.helpers.AssetManager;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 
 public class SplashScreen implements Screen {
     private final CashRun game;
@@ -24,7 +26,10 @@ public class SplashScreen implements Screen {
 
     private GameScreen gameScreen;
 
-    private final float iconSize = 600f;
+    private final float iconSize = 400f;
+    private final float thiefSize = iconSize * 2.2f;
+    private final float policeSize = iconSize * 1.8f;
+    private final float moneySize = iconSize * 1.5f;
     private final float titleSize = 2.5f;
 
     public SplashScreen(final CashRun game) {
@@ -34,13 +39,19 @@ public class SplashScreen implements Screen {
         camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
         Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
-        pixmap.setColor(0, 0, 0, 1);
+        pixmap.setColor(0, 0, 0, 0.5f);
         pixmap.fill();
         backgroundOverlay = new Texture(pixmap);
         pixmap.dispose();
 
 
-        font = new BitmapFont();
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/pixelifySans.ttf"));
+        FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+        parameter.size = 70;
+        parameter.color = Color.WHITE;
+        font = generator.generateFont(parameter);
+        generator.dispose();
+
         gameScreen = new GameScreen();
         Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
@@ -58,7 +69,7 @@ public class SplashScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 0.3f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
@@ -66,32 +77,40 @@ public class SplashScreen implements Screen {
 
         batch.begin();
 
-            /*fons transparent*/
-            batch.setColor(0, 0, 0, 0.7f);
-            batch.draw(backgroundOverlay, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-            batch.setColor(1, 1, 1, 1);
+        /*fons transparent*/
+        batch.setColor(0, 0, 0, 0.5f);
+        batch.draw(backgroundOverlay, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.setColor(1, 1, 1, 1);
 
-            float centerX = Gdx.graphics.getWidth() / 2f;
-            float centerY = Gdx.graphics.getHeight() / 2f;
+        float centerX = Gdx.graphics.getWidth() / 2f;
+        float centerY = Gdx.graphics.getHeight() / 2f;
 
-            float iconY = centerY ;
-            float iconSpacing = iconSize + 50f;
+        float iconY = centerY;
+        float spaceBetweenIcons = -90f;
 
-            batch.draw(AssetManager.policeRight[0], centerX - iconSpacing - iconSize / 2, iconY, iconSize, iconSize);
-            batch.draw(AssetManager.thiefRight[0], centerX - iconSize / 2, iconY, iconSize, iconSize);
-            batch.draw(AssetManager.coins, centerX + iconSpacing - iconSize / 2, iconY, iconSize, iconSize);
+        float thiefX = centerX - thiefSize / 2;
+        float policeX = thiefX - spaceBetweenIcons - policeSize;
+        float moneyX = thiefX + thiefSize + spaceBetweenIcons;
 
-            batch.draw(AssetManager.titleCashRun,
+        float thiefY = iconY + -155f;
+        float policeY = iconY + -65f;
+        float moneyY = iconY + -30;
+
+        batch.draw(AssetManager.policeRight[0], policeX, policeY, policeSize, policeSize);
+        batch.draw(AssetManager.thiefRight[0], thiefX, thiefY, thiefSize, thiefSize);
+        batch.draw(AssetManager.coins, moneyX, moneyY, moneySize, moneySize);
+
+
+        batch.draw(AssetManager.titleCashRun,
                 centerX - AssetManager.titleCashRun.getWidth() * titleSize / 2,
                 centerY - AssetManager.titleCashRun.getHeight() * titleSize / 2,
                 AssetManager.titleCashRun.getWidth() * titleSize,
                 AssetManager.titleCashRun.getHeight() * titleSize);
 
-        font.setColor(Color.WHITE);
-            font.getData().setScale(2f);
-            font.draw(batch, "Pulsa enter o clica para empezar", centerX - 150, centerY - 100);
+            font.setColor(Color.WHITE);
+            font.draw(batch, "Pulsa enter o clica para empezar", centerX - 550, centerY - 250);
 
-        batch.end();
+            batch.end();
     }
 
     @Override
